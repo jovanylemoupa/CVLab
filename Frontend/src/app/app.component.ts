e27b4b0905267209f3b84550116ef8e701c3d253
kurolarry <larry.tedou@gmail.com>
2026-04-26 13:29:46 +0200

    Revert "Feature/test show"

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Template01Component } from './features/template01/template01.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Template01Component],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Larry, Brel';
+}
